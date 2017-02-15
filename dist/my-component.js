96e3bf1b55161344ec73e6f637a2471b7588ca3b
katrialesser <katrialynn18@gmail.com>
2017-02-15 16:50:16 -0700

    adjusting styles, ran gulp to recompile

--- dist/my-component.js
+++ dist/my-component.js
@@ -17,26 +17,39 @@
 })(`<style>:host {
   display: inline-block;
   background-color: #fff;
-  padding: 15px;
+  padding: 0px;
   -moz-box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
   -webkit-box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
   box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
-  vertical-align: top; }
+  vertical-align: top;
+  min-width: 250px;
+  width: 30%;
+  max-width: 700px;
+  margin-bottom: 20px; }
 
-:host[columns='2'] .card {
+:host[columns='2'] {
   min-width: 250px;
   width: 45%;
   margin: 15px 2.5%; }
 
-:host[columns='3'] .card {
+:host[columns='3'] {
   min-width: 250px;
   width: 30%;
   margin: 15px 2%; }
 
-:host[columns='4'] .card {
+:host.columns-4,
+:host[columns='4'] {
   min-width: 250px;
   width: 23%;
   margin: 15px 1%; }
+
+:host img {
+  width: 100%;
+  padding: 0px; }
+
+:host h1, :host h2, :host h3, :host div, :host p {
+  width: 100%;
+  padding: 10px 15px; }
 
 @media screen and (max-width: 1024px) {
   :host[columns='2'] .card, :host[columns='3'] .card, :host[columns='4'] .card {
