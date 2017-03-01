720a0ea419f2ef9eb494c48aa8e171e4af8ec073
Katria Hale <katrialynn18@gmail.com>
2017-03-01 14:52:08 -0700

    doing rebuild w new files and theme change for time

---
+++ dist/components.es5.js
@@ -0,0 +1,149 @@
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+;
+
+(function () {
+    'use strict';
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    function _possibleConstructorReturn(self, call) {
+        if (!self) {
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        }
+
+        return call && ((typeof call === 'undefined' ? 'undefined' : _typeof(call)) === "object" || typeof call === "function") ? call : self;
+    }
+
+    function _inherits(subClass, superClass) {
+        if (typeof superClass !== "function" && superClass !== null) {
+            throw new TypeError("Super expression must either be null or a function, not " + (typeof superClass === 'undefined' ? 'undefined' : _typeof(superClass)));
+        }
+
+        subClass.prototype = Object.create(superClass && superClass.prototype, {
+            constructor: {
+                value: subClass,
+                enumerable: false,
+                writable: true,
+                configurable: true
+            }
+        });
+        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+    }
+
+    /******/(function (modules) {
+        // webpackBootstrap
+        /******/ // The module cache
+        /******/var installedModules = {};
+        /******/
+        /******/ // The require function
+        /******/function __webpack_require__(moduleId) {
+            /******/
+            /******/ // Check if module is in cache
+            /******/if (installedModules[moduleId])
+                /******/return installedModules[moduleId].exports;
+            /******/
+            /******/ // Create a new module (and put it into the cache)
+            /******/var module = installedModules[moduleId] = {
+                /******/i: moduleId,
+                /******/l: false,
+                /******/exports: {}
+                /******/ };
+            /******/
+            /******/ // Execute the module function
+            /******/modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+            /******/
+            /******/ // Flag the module as loaded
+            /******/module.l = true;
+            /******/
+            /******/ // Return the exports of the module
+            /******/return module.exports;
+            /******/
+        }
+        /******/
+        /******/
+        /******/ // expose the modules object (__webpack_modules__)
+        /******/__webpack_require__.m = modules;
+        /******/
+        /******/ // expose the module cache
+        /******/__webpack_require__.c = installedModules;
+        /******/
+        /******/ // identity function for calling harmony imports with the correct context
+        /******/__webpack_require__.i = function (value) {
+            return value;
+        };
+        /******/
+        /******/ // define getter function for harmony exports
+        /******/__webpack_require__.d = function (exports, name, getter) {
+            /******/if (!__webpack_require__.o(exports, name)) {
+                /******/Object.defineProperty(exports, name, {
+                    /******/configurable: false,
+                    /******/enumerable: true,
+                    /******/get: getter
+                    /******/ });
+                /******/
+            }
+            /******/
+        };
+        /******/
+        /******/ // getDefaultExport function for compatibility with non-harmony modules
+        /******/__webpack_require__.n = function (module) {
+            /******/var getter = module && module.__esModule ?
+            /******/function getDefault() {
+                return module['default'];
+            } :
+            /******/function getModuleExports() {
+                return module;
+            };
+            /******/__webpack_require__.d(getter, 'a', getter);
+            /******/return getter;
+            /******/
+        };
+        /******/
+        /******/ // Object.prototype.hasOwnProperty.call
+        /******/__webpack_require__.o = function (object, property) {
+            return Object.prototype.hasOwnProperty.call(object, property);
+        };
+        /******/
+        /******/ // __webpack_public_path__
+        /******/__webpack_require__.p = "";
+        /******/
+        /******/ // Load entry module and return exports
+        /******/return __webpack_require__(__webpack_require__.s = 0);
+        /******/
+    })(
+    /************************************************************************/
+    /******/[
+    /* 0 */
+    /***/function (module, exports, __webpack_require__) {
+
+        "use strict";
+
+        var ByuCard = function (_HTMLElement) {
+            _inherits(ByuCard, _HTMLElement);
+
+            function ByuCard() {
+                _classCallCheck(this, ByuCard);
+
+                var _this = _possibleConstructorReturn(this, (ByuCard.__proto__ || Object.getPrototypeOf(ByuCard)).call(this));
+
+                var shadowRoot = _this.attachShadow({ mode: 'open' });
+                shadowRoot.innerHTML = template;
+                return _this;
+            }
+
+            return ByuCard;
+        }(HTMLElement);
+
+        window.customElements.define('byu-card', ByuCard);
+        window.ByuCard = ByuCard;
+
+        /***/
+    }]);
+    //# sourceMappingURL=components.js.map
+})();
+//# sourceMappingURL=components.es5.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -15,34 +15,84 @@
  *    limitations under the License.
  **/
 'use strict';
-//const cached    = require('gulp-cached');
-const fuse      = require('gulp-fuse');
-const gulp      = require('gulp');
-const sass      = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const path = require('path');
+const pump = require('pump');
+const rename = require('gulp-rename');
+const through = require('through2');
+const sourcemaps = require('gulp-sourcemaps');
+const webpackStream = require('webpack-stream');
+const babel = require('gulp-babel');
+const gulpif = require('gulp-if');
 
-gulp.task('build', ['fuse'], function() {
-    // put closure code in here
+gulp.task('build', ['assemble', 'minify']);
+
+gulp.task('assemble', function () {
+    return gulp.src('./my-component/script.js')
+        .pipe(webpackStream(require('./webpack.config.js'), require('webpack')))
+        .pipe(gulp.dest('dist/'))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(through.obj(function (file, enc, cb) {
+            // Dont pipe through any source map files as it will be handled
+            // by gulp-sourcemaps
+            var isSourceMap = /\.map$/.test(file.path);
+            if (!isSourceMap) this.push(file);
+            cb();
+        }))
+        .pipe(babel({
+            presets: [
+                ['env', {
+                    "targets": {
+                        "browsers": ["last 2 versions", "ie >= 11", "> 5% in US"]
+                    }
+                }]
+            ],
+            plugins: ['iife-wrap']
+        }))
+        .pipe(rename({suffix: '.es5'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({stream: true}))
+        ;
 });
 
-gulp.task('fuse', ['sass'], function() {
-	/* Pulling the files of the component and bundling them into a single js file */
-    return gulp.src('./my-component/*')
-        //.pipe(cached('fuse'))
-        .pipe(fuse())
-        .pipe(gulp.dest('./dist'));
+gulp.task('minify', ['assemble'], function () {
+    return gulp.src(['dist/components.js','dist/components.es5.js'])
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(babel({
+            presets: ['babili']
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('sass', function() {
-	/* compiles the scss into css */
-    return gulp.src('./my-component/*.scss')
-        //.pipe(cached('sass'))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./my-component'));
+gulp.task('watch', ['assemble'], function (done) {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        notify: false,
+        startPath: "my-component/demo.html"
+        // proxy: {
+        //     target: 'http://localhost:3000',
+        //     middleware: function (req, res, next) {
+        //         //console.log(req.url);
+        //         res.setHeader('Access-Control-Allow-Origin', '*');
+        //         next();
+        //     }
+        // }
+
+    }, done);
+
+    gulp.watch(['index.html', './my-component/**', './css/*.scss'], ['assemble']);
+    gulp.watch(['webpack.config.js'], ['clear-webpack-cache', 'assemble'])
 });
 
-gulp.task('watch',['build'], function () {
-	/* whenever a file in my-component directory is changed, run the fuse task to re-bundle  */
-    gulp.watch('./my-component/*', ['build']);
+gulp.task('clear-webpack-cache', function() {
+    delete require.cache[path.resolve('./webpack.config.js')];
 });
 
 gulp.task('default', ['watch']);
+
