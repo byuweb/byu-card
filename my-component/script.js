dd329dd26a3180599d2a96cba6113d41988269ec
rburkey87 <rburkey87@gmail.com>
2017-04-03 16:18:10 -0600

    Shady footer updates and gulp build

--- my-component/script.js
+++ my-component/script.js
@@ -1,15 +1,23 @@
     'use strict';
+    const util = require('byu-web-component-utils');
     const template = require('./template.html');
+
     class ByuCard extends HTMLElement {
 
         constructor() {
             super();
-            let shadowRoot = this.attachShadow({mode: 'open'});
-            shadowRoot.innerHTML = template;
-		}
+            this.attachShadow({mode: 'open'});
+        }
 
     }
 
+    connectedCallback() {
+        util.applyTemplate(this, 'byu-card', template, () => {
+            // this._addSomeEventListenersOrSomething();
+        });
+    }
+
+    // this.shadowRoot.querySelector('#myId');
+
     window.customElements.define('byu-card', ByuCard);
     window.ByuCard = ByuCard;
-
